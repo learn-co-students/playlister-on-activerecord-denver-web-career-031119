bef9646b816fcd45e70e61de13b6042b5d6e4db7
github username <github email address>
2019-03-25 16:27:46 -0600

    Fixed it

--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -2,6 +2,8 @@ class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
       t.text :name
+      t.references :genre
+      t.references :artist
     end
   end
 end
