a615ed17bf6e28e8e97db42e300a01f5ea2ad4a5
github username <github email address>
2019-03-25 16:21:13 -0600

    Done.

--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[5.2]
+  def change
+    create_table 'songs' do |t|
+      t.text :name
+      t.references :artist
+      t.references :genre
+    end
+  end 
 end
