9101a829a3cceeffd39722724ef1f2520e7e0218
ibenthinkin <benjamin.reilly@gmail.com>
2019-03-25 15:49:10 -0600

    did the lab

--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :songs do |t|
+      t.integer :artist_id
+      t.integer :genre_id
+      t.text :name
+    end
+  end
 end
